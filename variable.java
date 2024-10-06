9b03a8f49496cd26999ba64a8a79174e4b20388c
Sameeksha Poojary <sameeksha.kombody@gmail.com>
2024-10-06 11:47:13 +0530

    updated

--- variable.java
+++ variable.java
@@ -1,8 +1,43 @@
-public class Variable
-{
-	public static void main(String args[])
-	{
-		int id;
-		String Name;
-	}
+public class variable{
+
+    public static void main(String[] args) {
+
+        // Integer data types
+        int populationOfCity = 1500000;
+        int distanceToMoonInKm = 384400;
+        int speedOfLightInKmPerSec = 299792;
+        int nationalDebt = 2147483647; // Max value of int
+        int followersOnSocialMedia = 50000;
+
+        // Long data types
+        long distanceToNearestStarInKm = 9460730472580L;
+        long worldPopulation = 7800000000L;
+        long numberOfStarsInMilkyWay = 250000000000L;
+        long nationalDebtInDollars = 22000000000000L;
+
+        // Float data types
+        float priceOfBread = 1.99f;
+        float piApproximation = 3.14f;
+        float temperatureInFahrenheit = 98.6f;
+        float distanceRunInKm = 5.75f;
+
+        // Double data types
+        double atomicMassOfCarbon = 12.011;
+        double goldenRatio = 1.61803398875;
+        double lightYearInKm = 9.4607e12;
+        double averageHumanWeightInKg = 70.5;
+
+        // Character data types
+        char gradeInExam = 'A';
+        char currencySymbol = '$';
+        char initialLetterOfName = 'S';
+        char gender = 'F';
+
+        // Printing some of these values
+        System.out.println("Population of city: " + populationOfCity);
+        System.out.println("Distance to the nearest star: " + distanceToNearestStarInKm + " km");
+        System.out.println("Price of bread: " + priceOfBread);
+        System.out.println("Atomic mass of carbon: " + atomicMassOfCarbon);
+        System.out.println("Grade in exam: " + gradeInExam);
+    }
 }
